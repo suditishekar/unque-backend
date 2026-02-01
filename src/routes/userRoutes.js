5891467d42c63a8a74f6ea3cb821e89cb40f9553
Suditi Shekar <suditi.shekar@gmail.com>
2026-02-01 15:29:31 +0530

    Add JWT authentication and availability model

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
@@ -46,6 +49,73 @@ router.post('/register', async (req, res) => {
     message: error.message
   });
 }
+});
+
+// Login user
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check required fields
+    if (!email || !password) {
+      return res.status(400).json({
+        message: 'Email and password are required'
+      });
+    }
+
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      });
+    }
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      });
+    }
+
+    // Successful login
+    // Create JWT token
+const token = jwt.sign(
+  {
+    userId: user._id,
+    role: user.role
+  },
+  process.env.JWT_SECRET,
+  {
+    expiresIn: '1h'
+  }
+);
+
+res.status(200).json({
+  message: 'Login successful',
+  token,
+  user: {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  }
+});
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server error'
+    });
+  }
+});
+
+// Protected test route-debugging
+router.get('/me', authMiddleware, (req, res) => {
+  res.json({
+    message: 'Access granted',
+    user: req.user
+  });
 });
 
 module.exports = router;
