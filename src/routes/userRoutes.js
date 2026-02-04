f6c9d2188f267687a9b5faeb1853312b73706068
Suditi Shekar <suditi.shekar@gmail.com>
2026-02-04 12:53:09 +0530

    Final cleanup

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
